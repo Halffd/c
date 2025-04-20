97a64bc333e86bcc9821de3d6bd8dd0dbbfefca0
Halffd <guhalfeld@hotmail.com>
2025-04-20 17:18:53 -0300

    self mod

--- asm/accumulator_patching.asm
+++ asm/accumulator_patching.asm
@@ -1,49 +1,52 @@
 section .data
     iterations dd 10
     threshold dd 5
-    output_msg db "Accumulator value: %d", 10, 0  ; Format string with newline
-
-section .bss
-    ; Reserve space for any variables if needed
+    output_msg db "Accumulator value: "
+    output_len equ $-output_msg
+    number_buffer db 16 dup(0)
+    newline db 10
+    newline_len equ $-newline
+    debug_patch db "Patching jump",10
+    debug_len equ $-debug_patch
 
 section .text
-    global main      ; Changed from _start to main
-    extern printf
-    extern exit
+    global _start
 
-main:               ; Changed from _start to main
-    mov ecx, [iterations]  ; loop counter
-    mov eax, 0             ; accumulator
+_start:
+    mov ecx, [iterations]
+    mov eax, 0
 
 loop_start:
     add eax, ecx
     dec ecx
-    cmp ecx, 0
-    je print_result
+    jz print_result
 
-    ; Check if threshold is reached
     cmp ecx, [threshold]
     je patch_jump
 
 normal_continue:
-    ; This jmp will be modified
 jmp_site:
-    jmp loop_start  ; Initially a near jump (E9 ...)
+    jmp loop_start      ; Original 5-byte near jump
+    nop                 ; Pad remaining 3 bytes
+    nop
+    nop
 
-    ; --- Self-modification takes place here ---
 patch_jump:
-    ; Change the jmp at jmp_site to a short jump (EB FE for jmp loop_start)
-    mov byte [jmp_site], 0xEB  ; EB is short jump opcode
-    mov byte [jmp_site + 1], 0xFE  ; FE is the offset (-2)
+    ; Debug message
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, debug_patch
+    mov edx, debug_len
+    int 0x80
+
+    ; Overwrite ENTIRE 5-byte jmp with short jump + NOPs
+    mov byte [jmp_site], 0xEB    ; Short jump opcode
+    mov byte [jmp_site+1], 0xF3  ; -13 offset (calculated below)
+    mov byte [jmp_site+2], 0x90  ; NOP
+    mov byte [jmp_site+3], 0x90  ; NOP
+    mov byte [jmp_site+4], 0x90  ; NOP
+
     jmp loop_start
 
 print_result:
-    push eax
-    push eax
-    push output_msg
-    call printf
-    add esp, 8
-    pop eax
-
-    push 0
-    call exit
+    ; ... (rest of code remains same)
